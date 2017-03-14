db61021ccc4bc35860aae230313ae0fb79ba8491
wanglz <wanglz@blizzmi.com>
2017-03-14 15:07:48 +0800

    Initial commit

---
+++ maven/src/main/java/com/blizzmi/maven/MavenUtils.java
@@ -0,0 +1,15 @@
+package com.blizzmi.maven;
+
+/**
+ * Date： 2017/3/14
+ * Description:
+ * maven test
+ *
+ * @author WangLizhi
+ * @version 1.0
+ */
+public class MavenUtils {
+    public static String test(){
+        return "maven test";
+    }
+}
